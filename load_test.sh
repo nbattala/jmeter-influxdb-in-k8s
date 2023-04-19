67a642a02e27e99291fb12638e07db65949701f8
Naveen Battala <naveen.battala@sas.com>
2023-04-19 10:51:54 -0700

    add load_test script

---
+++ load_test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+jmeter_ns=$(kubectl get pods -A -l app=jmeter -o=jsonpath='{.items[].metadata.namespace}')
+
+while getopts d:s o
+do	case "$o" in
+	t)	testfile="$OPTARG";;
+	[?])	print >&2 "Usage: $0 [-t <path to jmeter script>] [-<other jmeter test options>]"
+		exit 1;;
+	esac
+done
+shift $OPTIND-1
+
+if [ ! -f "$testfile" ]
+then
+    echo "ERROR: Jmeter Test script was not found in PATH. Please input the correct file path"
+    exit
+fi
+
+test_name=$(basename "$jmx")
+
+jmeter_pod=$(kubectl get po -n ${jmeter_ns} -l app=jmeter)
+
+#copy the script to jmeter pod
+kubectl cp "$jmx" -n ${jmeter_ns} "${jmeter_pod}:/${test_name}
+
+echo "starting jmeter load test"
+
+kubectl exec -it -n ${jmeter_ns} ${jmeter_pod} -- /bin/bash /load_test "$@"
+
