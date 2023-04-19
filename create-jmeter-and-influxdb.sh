ec29b9e86cd1cf0da78966675ac546e6c512ddb7
nabatt <naveen.battala@sas.com>
2023-04-19 01:43:24 -0400

    add influx and jmeter scripts

---
+++ create-jmeter-and-influxdb.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+namespace="$1"
+if [ -z "$namespace" ]
+then
+  echo "Enter the name of an existing or a new namespace to be used to create jmeter and influxdb"
+  read namespace
+fi
+
+kubectl create namespace $namespace --dry-run=client -o yaml | kubectl apply -f -
+
+echo "Creating Jmeter Deployment and Configmap"
+kubectl -n $namespace apply -f jmeter_cm.yaml
+
+kubectl -n $namespace apply -f jmeter_deploy.yaml
+
+echo "Creating Influxdb and the service"
+helm repo add influxdata https://helm.influxdata.com/
+helm upgrade -n $namespace --install influxdb-rel \
+  -f influxdb-helm-values.yaml \
+  influxdata/influxdb 
